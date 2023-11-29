df305324107249a460f69baa008d2afe10685d01
xzhu <xzhu@magmacapitalfunds.com>
2023-11-28 18:10:03 -0600

    add an example of a struct with memeber function

--- Chapter3/alignment.cpp
+++ Chapter3/alignment.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdint>
 #include <cstddef>
+#include <iostream>
 
 struct PoorlyAlignedData {
   char c;
@@ -34,6 +35,20 @@ struct PackedData2 {
 };
 #pragma pack(pop)
 
+#pragma pack(push, 1) // Set the packing alignment to 1 byte
+struct PackedStruct3 {
+    char c;       // 1 byte
+    uint16_t u;   // 2 bytes
+    double d;     // 8 bytes
+    int16_t i;    // 2 bytes
+
+    // Member function
+    void display() const {
+        std::cout << "c: " << c << ", u: " << u << ", d: " << d << "i: " << i << std::endl;
+    }
+};
+#pragma pack(pop) // Restore the previous packing alignment
+
 int main() {
   printf("PoorlyAlignedData c:%lu u:%lu d:%lu i:%lu size:%lu\n",
          offsetof(struct PoorlyAlignedData,c), offsetof(struct PoorlyAlignedData,u), offsetof(struct PoorlyAlignedData,d), offsetof(struct PoorlyAlignedData,i), sizeof(PoorlyAlignedData));
@@ -44,5 +59,13 @@ int main() {
          offsetof(struct PackedData,d), offsetof(struct PackedData,u), offsetof(struct PackedData,i), offsetof(struct PackedData,c), sizeof(PackedData));
   printf("PackedData2 d:%lu u:%lu i:%lu c:%lu size:%lu\n",
           offsetof(struct PackedData2,d), offsetof(struct PackedData2,u), offsetof(struct PackedData2,i), offsetof(struct PackedData2,c), sizeof(PackedData2));
-
+  PackedStruct3 ps3;
+  ps3.c = 'c';
+  ps3.d = 1.0;
+  ps3.i = 1;
+  ps3.u = 1;
+  ps3.display();
+  printf("PackedStruct3 c:%lu d:%lu i:%lu u:%lu size:%lu\n",
+          offsetof(struct PackedStruct3,c), offsetof(struct PackedStruct3,d), offsetof(struct PackedStruct3,i), offsetof(struct PackedStruct3,u), sizeof(PackedStruct3));
+  return 0;
 }
