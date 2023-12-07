4a903fe1ca6e9c9ffb2d2bd1622549ca637cf88f
xzhu <xzhu@magmacapitalfunds.com>
2023-12-07 10:31:16 -0600

    modification on cmakelists

--- Chapter6/build.sh
+++ Chapter6/build.sh
@@ -5,6 +5,7 @@ CMAKE=$(which cmake)
 NINJA=$(which ninja)
 
 mkdir -p cmake-build-release
+
 $CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B cmake-build-release
 
 $CMAKE --build cmake-build-release --target clean -j 4
--- Chapter8/scripts/build.sh
+++ Chapter8/scripts/build.sh
@@ -4,14 +4,20 @@
 CMAKE=$(which cmake)
 NINJA=$(which ninja)
 
-mkdir -p ./cmake-build-release
-$CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B ./cmake-build-release
+# Specify the source and build directories
+SOURCE_DIR="/home/xpzhu/project/Building-Low-Latency-Applications-with-CPP/Chapter8"
+BUILD_DIR="${SOURCE_DIR}/cmake-build-release"
+# Create the build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+$CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B "$BUILD_DIR"
 
-$CMAKE --build ./cmake-build-release --target clean -j 4
-$CMAKE --build ./cmake-build-release --target all -j 4
+$CMAKE --build "$BUILD_DIR" --target clean -j 4
+$CMAKE --build "$BUILD_DIR" --target all -j 4
 
-mkdir -p ./cmake-build-debug
-$CMAKE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B ./cmake-build-debug
+#create a debug build
+DEBUG_DIR="${SOURCE_DIR}/cmake-build-debug"
+mkdir -p "$DEBUG_DIR"
+$CMAKE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B "$DEBUG_DIR"
 
-$CMAKE --build ./cmake-build-debug --target clean -j 4
-$CMAKE --build ./cmake-build-debug --target all -j 4
+$CMAKE --build "$DEBUG_DIR" --target clean -j 4
+$CMAKE --build "$DEBUG_DIR" --target all -j 4
