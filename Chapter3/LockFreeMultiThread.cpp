c0ebd52370b5cae9fe88fd9b50f027706a1e1ba0
xzhu <xzhu@magmacapitalfunds.com>
2023-12-11 17:55:37 -0600

    comments for lock-free

--- Chapter3/LockFreeMultiThread.cpp
+++ Chapter3/LockFreeMultiThread.cpp
@@ -8,9 +8,14 @@ class BankAccount{
      public:
          BankAccount(): balance(0.0){}
          void deposit(double amount){
+            
             double currentBalance = balance.load(std::memory_order_relaxed);
+            // currentBalance: This is a reference to a 'double' variable that holds the current value of 'balance' before any update
+            // It's used both as the expected value for comparison and to store the updated value if the comparison succeeds.
+            // currentBalance + amount: This is the new value of the balance if the comparison succeeds. 
+            // std::memory_order_release: This is the memory order for the store operation if the comparison succeeds.
             while (!balance.compare_exchange_weak(currentBalance, currentBalance + amount, std::memory_order_release, std::memory_order_relaxed));
-            // Retry if the balance has changed.
+            // Retry if the balance has not changed.
          }
 
          void withdraw(double amount){
